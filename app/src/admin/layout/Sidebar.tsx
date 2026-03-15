f45e5c9f6dc2f52001bb2aba610e6948a9257ac1
Adrian Herscu <adrian.herscu@gmail.com>
2026-03-15 19:37:48 +0200

    refactor: remove settings menu item from sidebar and unused icons from user constants

--- app/src/admin/layout/Sidebar.tsx
+++ app/src/admin/layout/Sidebar.tsx
@@ -7,7 +7,6 @@ import {
   LayoutDashboard,
   LayoutTemplate,
   School,
-  Settings,
   Sheet,
   X,
 } from "lucide-react";
@@ -235,25 +234,6 @@ const Sidebar = ({ sidebarOpen, setSidebarOpen, userRole }: SidebarProps) => {
               )}
               {/* <!-- Menu Item Syllabuses --> */}
 
-              {/* <!-- Menu Item Settings --> */}
-              <li>
-                <NavLink
-                  to="/admin/settings"
-                  end
-                  className={({ isActive }) =>
-                    cn(
-                      "text-muted-foreground hover:bg-accent hover:text-accent-foreground group relative flex items-center gap-2.5 rounded-sm px-4 py-2 font-medium duration-300 ease-in-out",
-                      {
-                        "bg-accent text-accent-foreground": isActive,
-                      },
-                    )
-                  }
-                >
-                  <Settings />
-                  {t("admin.settings")}
-                </NavLink>
-              </li>
-              {/* <!-- Menu Item Settings --> */}
             </ul>
           </div>
 
--- app/src/user/constants.ts
+++ app/src/user/constants.ts
@@ -1,4 +1,4 @@
-import { GraduationCap, School, Settings, Shield } from "lucide-react";
+import { Settings } from "lucide-react";
 // import { LayoutDashboard } from "lucide-react";
 import { routes } from "wasp/client/router";
 
@@ -16,26 +16,5 @@ export const userMenuItems = [
     icon: Settings,
     isAuthRequired: false,
     isAdminOnly: false,
-  },
-  {
-    nameKey: "user.mySchool",
-    to: routes.ManagerSchoolRoute.to,
-    icon: School,
-    isAuthRequired: false,
-    isAdminOnly: true,
-  },
-  {
-    nameKey: "user.syllabuses",
-    to: routes.ManagerSyllabusesRoute.to,
-    icon: GraduationCap,
-    isAuthRequired: false,
-    isAdminOnly: true,
-  },
-  {
-    nameKey: "user.adminDashboard",
-    to: routes.AdminRoute.to,
-    icon: Shield,
-    isAuthRequired: false,
-    isAdminOnly: true,
   },
 ] as const;
